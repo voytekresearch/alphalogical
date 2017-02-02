06097286866446aa7d2e49acd5bfb90add849069
Erik Peterson <Erik.Exists@gmail.com>
2017-02-01 17:56:57 -0800

    remove test code

--- util.py
+++ util.py
@@ -99,37 +99,3 @@ def find_bursts2(x, burst_thresh, zero_thresh, history=200):
             b = False
 
     return bursts
-
-
-if __name__ == "__main__":
-    from scipy.io import loadmat
-    from scipy.signal import hilbert
-    from alphalogical.util import *
-
-    fs = 1000
-    t = 120  # seconds long
-    alpha = loadmat('data/alpha_data.mat')
-    times = np.linspace(0, 120, 120 * 500)
-
-    task = alpha['oz_dat_task'][0, :]
-
-    # smooth a little
-
-    data = butter_bandpass_filter(task, .1, 30, fs, order=2)
-
-    data_f = butter_bandpass_filter(data, 8, 12, fs, order=2)
-    data_pow = np.abs(hilbert(data_f))
-
-    low = np.abs(hilbert(butter_bandpass_filter(data, 5, 7, fs, order=2)))
-    high = np.abs(hilbert(butter_bandpass_filter(data, 14, 16, fs, order=2)))
-    base = (low + high) / 2.0
-
-    task = butter_bandpass_filter(task, .1, 30, 500, order=2)
-
-    # Find burst indices
-    start = 1.5
-    stop = 1.0
-    task_b = find_bursts(data_pow, base.mean() * start, base.mean() * stop)
-
-    # Burst stats
-    print(len(task_b))
